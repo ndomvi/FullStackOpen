bb29a382fed3fd692f2edf413951224904eaede6
ndomvi <ndomvi@gmail.com>
2019-11-07 19:44:14 +0200

    part1 b

---
+++ part1/src/index.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = props => {
+  const { text } = props
+  return (
+    <div>
+      <h1>{text}</h1>
+    </div>
+  )
+}
+
+const Content = props => {
+  const { parts } = props
+  return (
+    <div>
+      <Part part={parts[0].name} exercises={parts[0].exercises} />
+      <Part part={parts[1].name} exercises={parts[1].exercises} />
+      <Part part={parts[2].name} exercises={parts[2].exercises} />
+    </div>
+  )
+}
+
+const Part = props => {
+  const { part, exercises } = props
+  return (
+    <>
+      <p>
+        {part} {exercises}
+      </p>
+    </>
+  )
+}
+
+const Total = props => {
+  const { parts } = props
+  return (
+    <div>
+      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header text={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
